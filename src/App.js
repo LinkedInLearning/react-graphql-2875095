adfa21f75a702682ded02bb5e0ac39053d16de86
Ray Villalobos <ray@planetoftheweb.com>
2021-01-22 18:47:41 -0800

    retrieved the data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import github from "./db.js";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const githubQuery = {
+      query: `
+      {
+        viewer {
+          name
+        }
+      }
+    `,
+    };
+
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(githubQuery),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
