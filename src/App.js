475f521d854a3338e74d245141e29ad7474bcf47
Ray Villalobos <ray@planetoftheweb.com>
2021-01-24 20:18:56 -0800

    Use Callback Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import github from "./db.js";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import query from "./Query";
 
 function App() {
   let [userName, setUserName] = useState("");
 
-  useEffect(() => {
+  const fetchData = useCallback(() => {
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
@@ -19,7 +19,11 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div className="App container mt-5">
